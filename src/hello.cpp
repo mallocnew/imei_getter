4d2c7fc50505365775e40f868b6933837609e590
fei.hao <fei.hao@mobvoi.com>
2021-09-09 21:06:37 +0800

    Get IMEI by lambda

--- src/hello.cpp
+++ src/hello.cpp
@@ -19,8 +19,18 @@ std::string exec(const char* cmd) {
 int main() {
   try {
     //    system("service call iphonesubinfo 1 >/sdcard/mobvoi/imei.txt");
-
-    string imei_origin = exec("service call iphonesubinfo 1");
+    auto exec_android = [](const char* cmd) {
+      std::array<char, 128> buffer;
+      std::string result;
+      std::shared_ptr<FILE> pipe(popen(cmd, "r"), pclose);
+      if (!pipe) throw std::runtime_error("popen() failed!");
+      while (!feof(pipe.get())) {
+        if (fgets(buffer.data(), 128, pipe.get()) != nullptr)
+          result += buffer.data();
+      }
+      return result;
+    };
+    string imei_origin = exec_android("service call iphonesubinfo 1");
     cout << imei_origin << endl;
     int indexes[] = {75,  77,  79,  81,  136, 138, 140, 142,
                      144, 146, 148, 150, 205, 207, 209};
